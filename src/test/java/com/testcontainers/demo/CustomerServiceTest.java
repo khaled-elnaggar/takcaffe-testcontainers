6daee7f8bcd11af4a8759dd299b5cc56b1929a7a
Khaled Elnaggar <dev_khalid@hotmail.com>
2024-07-10 18:37:31 +0300

    Stabilize mysql demo

--- src/test/java/com/testcontainers/demo/CustomerServiceTest.java
+++ src/test/java/com/testcontainers/demo/CustomerServiceTest.java
@@ -13,9 +13,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CustomerServiceTest {
 
-
-//  ===========================================
-
   @Autowired
   CustomerService customerService;
 
